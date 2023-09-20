6981091bfb1684859d1e9a5693841cc5b6f948e9
Promtech <uwagboepromise5@gmail.com>
2023-09-20 13:50:21 +0100

    Updated swiper

--- src/app.js
+++ src/app.js
@@ -10,42 +10,6 @@ document.getElementById('form').addEventListener('submit', function () {
   alert("THANK YOU!");
 }, false);
 
-// Owlcarousel
-$(document).ready(function () {
-  var owl = $(".owl-carousel");
-  owl.owlCarousel({
-    margin: 0,
-    nav: false,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        margin: 20,
-      },
-      600: {
-        items: 2,
-        nav: true,
-      },
-      999: {
-        items: 3,
-        nav: true,
-      },
-      1200: {
-        items: 4,
-        nav: true,
-      },
-    },
-  });
-  $(".play").on("click", function () {
-    owl.trigger("play.owl.autoplay", [1000]);
-  });
-  $(".stop").on("click", function () {
-    owl.trigger("stop.owl.autoplay");
-  });
-});
 
 //submit btn
 function handleSubmit(event) {
